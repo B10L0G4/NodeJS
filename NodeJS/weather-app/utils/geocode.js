5556ecbc9a913739b4640dba2289067f5cef48bc
Vanessa Carraro <81984134+B1olog4@users.noreply.github.com>
2022-07-09 16:39:48 -0300

    Add files via upload

--- NodeJS/weather-app/utils/geocode.js
+++ NodeJS/weather-app/utils/geocode.js
@@ -2,17 +2,19 @@ const request = require('postman-request')
 
 const geocode = (address, callback) => {
     const url = 'http://api.positionstack.com/v1/forward?access_key=0393269fe71464ec869fa5c968233972&query='+address
-        request({ url: url, json: true }, (error, response) => {
+        request({ url, json: true }, (error, {body}) => {
         if (error) {
             callback('Unable to connect', undefined)
-        } else if (response.body.data[0].length === 0) {
+        } else if (body.data[0].length === 0) {
             callback('Unable to find location, try another search', undefined)
         } else {
             callback(undefined, {
-                latitude: response.body.data[0].latitude,
-                longitude: response.body.data[0].longitude,
-                location: response.body.data[0].label
+                latitude: body.data[0].latitude,
+                longitude:body.data[0].longitude,
+                location: body.data[0].region, 
+                location: body.data[0].country
             })
+            
         }
     })
 }

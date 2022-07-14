2d2d67ddff1f27e3904589b3b3c21993d57e74a5
Vanessa Carraro <81984134+B1olog4@users.noreply.github.com>
2022-07-14 09:26:41 -0300

    View directories

--- web-serve/src/app.js
+++ web-serve/src/app.js
@@ -1,10 +1,41 @@
+
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
+
 const app = express()
-
+// define caminhos para as configuracoes do express
 const publicDirectoryPath = path.join(__dirname, '../public') 
-app.use(express.static(publicDirectoryPath))
+const viewsPath = path.join(__dirname,'../template/views')
+const partialsPath = path.join(__dirname, '../template/partial')
 
+// setup handbars engine e visualizacao de locais 
+app.set('view engine','hbs')
+app.set('views', viewsPath)
+
+// setup estatico para o diretorio do servidor 
+app.use(express.static(publicDirectoryPath))
+hbs.registerPartials(partialsPath)
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather App',
+        name:'Vaness.C.Carraro'
+    })
+})
+app.get ('/about',(req,res)=>{
+    res.render('about',{
+        title:'About Weather App',
+        name:'Vanessa.C.Carraro'
+    })   
+})
+app.get ('/help',(req,res)=>{
+    res.render('help',{
+        title:'Help',
+        name:'Vanessa.C.Carraro',
+        textHelp:'Help test for Weather App'
+    })
+})
 app.get('/weather',(req, res )=> {
     res.send({
         forecast: 'Temperature Now is 20  ' ,

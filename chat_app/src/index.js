ad433e8febeb2cb4edd4af0f03952d476b004d86
Vanessa Cunha Carraro <vanessacarraro@ibm.com>
2022-12-21 04:37:24 -0300

    socket io configuration

--- chat_app/src/index.js
+++ chat_app/src/index.js
@@ -1,13 +1,24 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 
 const app = express()
+const server = http.createServer(app)
+const io = new socketio.Server()
+io.attach(server)
+
 const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
-app.listen(port, () => {
+
+io.on('connection', () => {
+    console.log('New Websocket connected ')
+})
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}!`)
 })

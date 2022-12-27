6ea78416209c416ee21494cf9b8b225c7afa2b0e
Vanessa Cunha Carraro <vanessacarraro@ibm.com>
2022-12-27 17:08:39 -0300

    add socketio

--- chat_app/src/index.js
+++ chat_app/src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const { count } = require('console')
 
 
 const app = express()
@@ -15,8 +16,9 @@ const publicDirectoryPath = path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('New Websocket connected ')
+    socket.emit('countUpdated',count)
 })
 // teste de commit 
 
